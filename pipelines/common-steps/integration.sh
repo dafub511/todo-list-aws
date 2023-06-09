3f035adb0a14deffa5a56f5cec38c1d7da5a303b
dafub511 <131792163+dafub511@users.noreply.github.com>
2023-06-10 00:17:38 +0200

    Update integration.sh

--- pipelines/common-steps/integration.sh
+++ pipelines/common-steps/integration.sh
@@ -3,4 +3,4 @@
 source todo-list-aws/bin/activate
 set -x
 export BASE_URL=https://9o2chw1e2i.execute-api.us-east-1.amazonaws.com/Prod
-python3.6 -m pytest -s test/integration/todoApiTest.py
+python3.7 -m pytest -s test/integration/todoApiTest.py
