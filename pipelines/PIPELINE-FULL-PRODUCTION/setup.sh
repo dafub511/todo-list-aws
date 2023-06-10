409c69a88cf89c9fcbefd2daec65c5a6f729a8ff
dafub511 <131792163+dafub511@users.noreply.github.com>
2023-06-10 02:58:57 +0200

    Update setup.sh

--- pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
+++ pipelines/PIPELINE-FULL-PRODUCTION/setup.sh
@@ -3,9 +3,9 @@
 set -x
 python3.7 -m venv todo-list-aws
 source todo-list-aws/bin/activate
-python -m pip install --upgrade pip
-python -m pip install awscli
-python -m pip install aws-sam-cli
+python3.7 -m pip install --upgrade pip
+python3.7 -m pip install awscli
+python3.7 -m pip install aws-sam-cli
 # For integration testing
-python -m pip install pytest
+python3.7 -m pip install pytest
 pwd
