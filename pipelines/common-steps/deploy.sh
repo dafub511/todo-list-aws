7536f1b35881166d39701b3d2e2a96d33e31b023
dafub511 <131792163+dafub511@users.noreply.github.com>
2023-06-11 03:32:52 +0200

    Update deploy.sh

--- pipelines/common-steps/deploy.sh
+++ pipelines/common-steps/deploy.sh
@@ -2,4 +2,5 @@
 
 set -x
 du -hs * | sort -h
-sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --resolve-s3
+sam deploy template.yaml --config-env ${ENVIRONMENT} --no-confirm-changeset --force-upload --no-fail-on-empty-changeset --no-progressbar --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-15d0sq06jl9ju
+
