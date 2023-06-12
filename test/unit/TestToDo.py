10e6f46a8b48d4bfc425a9d44ba2acca95e30eec
dafub511 <131792163+dafub511@users.noreply.github.com>
2023-06-12 18:58:33 +0200

    Update TestToDo.py

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -3,40 +3,12 @@ import warnings
 import unittest
 import boto3
 from moto import mock_dynamodb
+import sys
 import os
 import json
 
 @mock_dynamodb
-class TestTableExists(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        warnings.filterwarnings(
-            "ignore",
-            category=ResourceWarning,
-            message="unclosed.*<socket.socket.*>")
-        warnings.filterwarnings(
-            "ignore",
-            category=DeprecationWarning,
-            message="callable is None.*")
-        warnings.filterwarnings(
-            "ignore",
-            category=DeprecationWarning,
-            message="Using or importing.*")
-
-    def setUp(self):
-        self.dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-        from src.todoList import create_todo_table
-        self.table = create_todo_table(self.dynamodb)
-
-    def tearDown(self):
-        self.table.delete()
-        self.dynamodb = None
-
-    def test_table_exists(self):
-        tableName = os.environ['DYNAMODB_TABLE']
-        self.assertIn(tableName, self.table.name)
-
-class TestTodoFunctions(unittest.TestCase):
+class TestDatabaseFunctions(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         warnings.filterwarnings(
@@ -57,13 +29,16 @@ class TestTodoFunctions(unittest.TestCase):
         self.uuid = "123e4567-e89b-12d3-a456-426614174000"
         self.text = "Aprender DevOps y Cloud en la UNIR"
 
-        from src.todoList import create_todo_table, put_item
+        from src.todoList import create_todo_table
         self.table = create_todo_table(self.dynamodb)
-        put_item(self.text, self.dynamodb)
 
     def tearDown(self):
         self.table.delete()
         self.dynamodb = None
+
+    def test_table_exists(self):
+        tableName = os.environ['DYNAMODB_TABLE']
+        self.assertIn(tableName, self.table.name)
 
     def test_put_todo(self):
         from src.todoList import put_item
@@ -72,40 +47,53 @@ class TestTodoFunctions(unittest.TestCase):
 
     def test_put_todo_error(self):
         from src.todoList import put_item
-        self.assertRaises(Exception, put_item, "", self.dynamodb)
+        with self.assertRaises(Exception):
+            put_item("", self.dynamodb)
 
     def test_get_todo(self):
-        from src.todoList import get_item
-        responseGet = get_item(self.uuid, self.dynamodb)
+        from src.todoList import get_item, put_item
+        responsePut = put_item(self.text, self.dynamodb)
+        idItem = json.loads(responsePut['body'])['id']
+        responseGet = get_item(idItem, self.dynamodb)
         self.assertEqual(self.text, responseGet['text'])
 
     def test_list_todo(self):
-        from src.todoList import get_items
+        from src.todoList import put_item, get_items
+        put_item(self.text, self.dynamodb)
         result = get_items(self.dynamodb)
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]['text'], self.text)
 
     def test_update_todo(self):
-        from src.todoList import update_item
+        from src.todoList import put_item, update_item, get_item
         updated_text = "Aprender más cosas que DevOps y Cloud en la UNIR"
-        result = update_item(self.uuid, updated_text, "false", self.dynamodb)
+        responsePut = put_item(self.text, self.dynamodb)
+        idItem = json.loads(responsePut['body'])['id']
+        result = update_item(idItem, updated_text, "false", self.dynamodb)
         self.assertEqual(result['text'], updated_text)
 
     def test_update_todo_error(self):
-        from src.todoList import update_item
+        from src.todoList import put_item, update_item
         updated_text = "Aprender más cosas que DevOps y Cloud en la UNIR"
-        self.assertRaises(Exception, update_item, updated_text, "", "false", self.dynamodb)
-        self.assertRaises(TypeError, update_item, "", self.uuid, "false", self.dynamodb)
-        self.assertRaises(Exception, update_item, updated_text, self.uuid, "", self.dynamodb)
+        responsePut = put_item(self.text, self.dynamodb)
+        with self.assertRaises(Exception):
+            update_item(updated_text, "", "false", self.dynamodb)
+        with self.assertRaises(TypeError):
+            update_item("", self.uuid, "false", self.dynamodb)
+        with self.assertRaises(Exception):
+            update_item(updated_text, self.uuid, "", self.dynamodb)
 
     def test_delete_todo(self):
-        from src.todoList import delete_item, get_items
-        delete_item(self.uuid, self.dynamodb)
+        from src.todoList import delete_item, put_item, get_items
+        responsePut = put_item(self.text, self.dynamodb)
+        idItem = json.loads(responsePut['body'])['id']
+        delete_item(idItem, self.dynamodb)
         self.assertEqual(len(get_items(self.dynamodb)), 0)
 
     def test_delete_todo_error(self):
         from src.todoList import delete_item
-        self.assertRaises(TypeError, delete_item, "", self.dynamodb)
+        with self.assertRaises(TypeError):
+            delete_item("", self.dynamodb)
 
 if __name__ == '__main__':
     unittest.main()
